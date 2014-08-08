361f5f5e625194c1b0e208209ebb6185a96ad7f0
Xiang Li <xiang.li@coreos.com>
2014-08-08 10:23:07 -0700

    etcd: remove skipped test

--- etcd/etcd_functional_test.go
+++ etcd/etcd_functional_test.go
@@ -281,66 +281,6 @@ func BenchmarkEndToEndSet(b *testing.B) {
 	destoryCluster(nil, es, hs)
 }
 
-// TODO(yichengq): cannot handle previous msgDenial correctly now
-func TestModeSwitch(t *testing.T) {
-	t.Skip("not passed")
-	size := 5
-	round := 3
-
-	for i := 0; i < size; i++ {
-		es, hs := buildCluster(size, false)
-
-		config := config.NewClusterConfig()
-		config.SyncInterval = 0
-		id := int64(i)
-		for j := 0; j < round; j++ {
-			lead, _ := waitActiveLeader(es)
-			// cluster only demotes follower
-			if lead == id {
-				continue
-			}
-
-			config.ActiveSize = size - 1
-			if err := es[lead].p.setClusterConfig(config); err != nil {
-				t.Fatalf("#%d: setClusterConfig err = %v", i, err)
-			}
-			if err := es[lead].p.remove(id); err != nil {
-				t.Fatalf("#%d: remove err = %v", i, err)
-			}
-
-			if err := waitMode(standbyMode, es[i], nil); err != nil {
-				t.Fatalf("#%d: waitMode err = %v", i, err)
-			}
-
-			for k := 0; k < 4; k++ {
-				if es[i].s.leader != noneId {
-					break
-				}
-				time.Sleep(20 * time.Millisecond)
-			}
-			if g := es[i].s.leader; g != lead {
-				t.Errorf("#%d: lead = %d, want %d", i, g, lead)
-			}
-
-			config.ActiveSize = size
-			if err := es[lead].p.setClusterConfig(config); err != nil {
-				t.Fatalf("#%d: setClusterConfig err = %v", i, err)
-			}
-
-			if err := waitMode(participantMode, es[i], nil); err != nil {
-				t.Fatalf("#%d: waitMode err = %v", i, err)
-			}
-
-			if err := checkParticipant(i, es); err != nil {
-				t.Errorf("#%d: check alive err = %v", i, err)
-			}
-		}
-
-		destoryCluster(t, es, hs)
-	}
-	afterTest(t)
-}
-
 // Sending set commands
 func keepSetting(urlStr string, stop chan bool) {
 	tc := NewTestClient()
--- etcd/etcd_test.go
+++ etcd/etcd_test.go
@@ -595,27 +595,6 @@ func waitMode(mode int64, e *Server, errC chan error) error {
 	return fmt.Errorf("waitMode timeout 50ms")
 }
 
-// checkParticipant checks the i-th server works well as participant.
-func checkParticipant(i int, es []*Server) error {
-	lead, _ := waitActiveLeader(es)
-	key := fmt.Sprintf("/%d", rand.Int31())
-	ev, err := es[lead].p.Set(key, false, "bar", store.Permanent)
-	if err != nil {
-		return err
-	}
-
-	w, err := es[i].p.Watch(key, false, false, ev.Index())
-	if err != nil {
-		return err
-	}
-	select {
-	case <-w.EventChan:
-	case <-time.After(8 * defaultHeartbeat * es[i].tickDuration):
-		return fmt.Errorf("watch timeout")
-	}
-	return nil
-}
-
 func newTestConfig() *config.Config {
 	c := config.New()
 	c.Addr = "127.0.0.1:0"
